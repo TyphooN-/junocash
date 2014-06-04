TyphooN-/junocash

commit 020a236a2406531bfdaf095637da6588c2a4b83e

ProcessGetData(): abort if a block file is missing from disk

src/main.cpp
@@ -3373,7 +3373,7 @@ void static ProcessGetData(CNode* pfrom)
                 {
                     // Send block from disk
                     CBlock block;
-                    ReadBlockFromDisk(block, (*mi).second);
+                    assert(ReadBlockFromDisk(block, (*mi).second));
                     if (inv.type == MSG_BLOCK)
                         pfrom->PushMessage("block", block);
                     else // MSG_FILTERED_BLOCK)

commit 245e7d4822923c5496e732213f0651a3dce33344

Remove side effect in assertion in ProcessGetData
A side-effect was introduced into an assertion in 020a236. This commit
fixes that.

src/main.cpp
@@ -3390,7 +3390,8 @@ void static ProcessGetData(CNode* pfrom)
                 {
                     // Send block from disk
                     CBlock block;
-                    assert(ReadBlockFromDisk(block, (*mi).second));
+                    if (!ReadBlockFromDisk(block, (*mi).second))
+                        assert(!"cannot load block from disk");
                     if (inv.type == MSG_BLOCK)
                         pfrom->PushMessage("block", block);
                     else // MSG_FILTERED_BLOCK)