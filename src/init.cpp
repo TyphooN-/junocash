TyphooN-/junocash

commit 2dca7fa1466589364a264b4b292a634498a3044b

Attempt to log before terminating if prevector allocation fails

src/init.cpp
@@ -723,20 +723,6 @@ bool AppInitServers(boost::thread_group& threadGroup)
     return true;
 }
 
-[[noreturn]] static void new_handler_terminate()
-{
-    // Rather than throwing std::bad-alloc if allocation fails, terminate
-    // immediately to (try to) avoid chain corruption.
-    // Since LogPrintf may itself allocate memory, set the handler directly
-    // to terminate first.
-    std::set_new_handler(std::terminate);
-    fputs("Error: Out of memory. Terminating.\n", stderr);
-    LogPrintf("Error: Out of memory. Terminating.\n");
-
-    // The log was successful, terminate now.
-    std::terminate();
-};
-
 /** Initialize bitcoin.
  *  @pre Parameters should be parsed and config file should be read.
  */

src/prevector.h
@@ -1,6 +1,8 @@
 #ifndef _BITCOIN_PREVECTOR_H_
 #define _BITCOIN_PREVECTOR_H_
 
+#include <util.h>
+
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -171,11 +173,11 @@ private:
                     success. These should instead use an allocator or new/delete so that handlers
                     are called as necessary, but performance would be slightly degraded by doing so. */
                 _union.indirect = static_cast<char*>(realloc(_union.indirect, ((size_t)sizeof(T)) * new_capacity));
-                assert(_union.indirect);
+                if (!_union.indirect) { new_handler_terminate(); }
                 _union.capacity = new_capacity;
             } else {
                 char* new_indirect = static_cast<char*>(malloc(((size_t)sizeof(T)) * new_capacity));
-                assert(new_indirect);
+                if (!new_indirect) { new_handler_terminate(); }
                 T* src = direct_ptr(0);
                 T* dst = reinterpret_cast<T*>(new_indirect);
                 memcpy(dst, src, size() * sizeof(T));

src/util.h
@@ -53,6 +53,8 @@ extern bool fLogIPs;
 extern std::atomic<bool> fReopenDebugLog;
 extern CTranslationInterface translationInterface;
 
+[[noreturn]] extern void new_handler_terminate();
+
 /**
  * Translation function: Call Translate signal on UI interface, which returns a boost::optional result.
  * If no translation slot is registered, nothing is returned, and simply return the input.

src/util.cpp
@@ -17,6 +17,7 @@
 #include "utiltime.h"
 
 #include <stdarg.h>
+#include <stdio.h>
 
 #if (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__))
 #include <pthread.h>
@@ -178,6 +179,20 @@ static boost::once_flag debugPrintInitFlag = BOOST_ONCE_INIT;
 static FILE* fileout = NULL;
 static boost::mutex* mutexDebugLog = NULL;
 static list<string> *vMsgsBeforeOpenLog;
+
+[[noreturn]] void new_handler_terminate()
+{
+    // Rather than throwing std::bad-alloc if allocation fails, terminate
+    // immediately to (try to) avoid chain corruption.
+    // Since LogPrintf may itself allocate memory, set the handler directly
+    // to terminate first.
+    std::set_new_handler(std::terminate);
+    fputs("Error: Out of memory. Terminating.\n", stderr);
+    LogPrintf("Error: Out of memory. Terminating.\n");
+
+    // The log was successful, terminate now.
+    std::terminate();
+};
 
 static int FileWriteStr(const std::string &str, FILE *fp)
 {

commit 5c655f30284f84787795902078294a235d68cee7

prevector: Terminate without logging on failed allocation
This reverts 2dca7fa1466589364a264b4b292a634498a3044b, which introduced
logging, adding a dependency on libbitcoin_util.a to libzcashconsensus.a.

Also adds missing #includes that were being indirectly included via
prevector.h including util.h.

src/init.cpp
@@ -828,6 +828,20 @@ void InitLogging()
     LogPrintf("Zcash version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
 }
 
+[[noreturn]] static void new_handler_terminate()
+{
+    // Rather than throwing std::bad-alloc if allocation fails, terminate
+    // immediately to (try to) avoid chain corruption.
+    // Since LogPrintf may itself allocate memory, set the handler directly
+    // to terminate first.
+    std::set_new_handler(std::terminate);
+    fputs("Error: Out of memory. Terminating.\n", stderr);
+    LogPrintf("Error: Out of memory. Terminating.\n");
+
+    // The log was successful, terminate now.
+    std::terminate();
+};
+
 /** Initialize bitcoin.
  *  @pre Parameters should be parsed and config file should be read.
  */

src/logging.cpp
@@ -54,20 +54,6 @@ static FILE* fileout = NULL;
 static boost::mutex* mutexDebugLog = NULL;
 static list<string> *vMsgsBeforeOpenLog;
 
-[[noreturn]] void new_handler_terminate()
-{
-    // Rather than throwing std::bad-alloc if allocation fails, terminate
-    // immediately to (try to) avoid chain corruption.
-    // Since LogPrintf may itself allocate memory, set the handler directly
-    // to terminate first.
-    std::set_new_handler(std::terminate);
-    fputs("Error: Out of memory. Terminating.\n", stderr);
-    LogPrintf("Error: Out of memory. Terminating.\n");
-
-    // The log was successful, terminate now.
-    std::terminate();
-};
-
 static int FileWriteStr(const std::string &str, FILE *fp)
 {
     return fwrite(str.data(), 1, str.size(), fp);

src/mempool_limit.cpp
@@ -2,11 +2,14 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php .
 
-#include "core_memusage.h"
 #include "mempool_limit.h"
+
+#include "core_memusage.h"
+#include "logging.h"
 #include "random.h"
 #include "serialize.h"
 #include "timedata.h"
+#include "utiltime.h"
 #include "version.h"
 
 const TxWeight ZERO_WEIGHT = TxWeight(0, 0);

src/miner.h
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include <variant>
 
+#include <boost/shared_ptr.hpp>
+
 class CBlockIndex;
 class CChainParams;
 class CScript;

src/prevector.h
@@ -5,8 +5,6 @@
 #ifndef BITCOIN_PREVECTOR_H
 #define BITCOIN_PREVECTOR_H
 
-#include <util.h>
-
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -177,11 +175,11 @@ private:
                     success. These should instead use an allocator or new/delete so that handlers
                     are called as necessary, but performance would be slightly degraded by doing so. */
                 _union.indirect = static_cast<char*>(realloc(_union.indirect, ((size_t)sizeof(T)) * new_capacity));
-                if (!_union.indirect) { new_handler_terminate(); }
+                assert(_union.indirect);
                 _union.capacity = new_capacity;
             } else {
                 char* new_indirect = static_cast<char*>(malloc(((size_t)sizeof(T)) * new_capacity));
-                if (!new_indirect) { new_handler_terminate(); }
+                assert(new_indirect);
                 T* src = direct_ptr(0);
                 T* dst = reinterpret_cast<T*>(new_indirect);
                 memcpy(dst, src, size() * sizeof(T));

src/util.h
@@ -34,8 +34,6 @@ extern std::map<std::string, std::vector<std::string> > mapMultiArgs;
 extern bool fDebug;
 extern bool fServer;
 
-[[noreturn]] extern void new_handler_terminate();
-
 extern const char * const BITCOIN_CONF_FILENAME;
 extern const char * const BITCOIN_PID_FILENAME;
 

src/zcash/Note.cpp
@@ -1,7 +1,9 @@
 #include "Note.hpp"
+
 #include "prf.h"
 #include "crypto/sha256.h"
 #include "consensus/consensus.h"
+#include "logging.h"
 
 #include "random.h"
 #include "version.h"
@@ -9,6 +11,8 @@
 
 #include "zcash/util.h"
 #include "librustzcash.h"
+
+#include <boost/thread/exceptions.hpp>
 
 using namespace libzcash;
 

src/gtest/test_timedata.cpp
@@ -8,6 +8,7 @@
 #include "timedata.h"
 #include "random.h"
 #include "netbase.h"
+#include "utiltime.h"
 
 using ::testing::StrictMock;
 