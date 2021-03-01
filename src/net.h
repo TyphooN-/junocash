TyphooN-/junocash

commit df7740fdda9f83ab1fd950e2d6a82bcf6beddbb9

Make fDisconnect an std::atomic

src/net.h
@@ -287,7 +287,7 @@ public:
     bool fInbound;
     bool fNetworkNode;
     bool fSuccessfullyConnected;
-    bool fDisconnect;
+    std::atomic_bool fDisconnect;
     // We use fRelayTxes for two purposes -
     // a) it allows us to not relay tx invs before receiving the peer's version message
     // b) the peer may tell us in its version message that we should not relay tx invs

commit 5c7ab2a216f3ce5762f364d79c95f15a381c3075

Fix regression introduced in df7740fdda9f83ab1fd950e2d6a82bcf6beddbb9 by adding #include <atomic>. fixes #5014
Signed-off-by: Daira Hopwood <daira@jacaranda.org>

src/net.h
@@ -23,6 +23,7 @@
 
 #include <deque>
 #include <stdint.h>
+#include <atomic>
 
 #ifndef WIN32
 #include <arpa/inet.h>